aac242cf7249982e8100e409e2d30ba86d034f13
ChandraShekhar <csyadav0513@gmail.com>
2024-07-25 18:36:58 +0530

    Add logout route and functionality

---
+++ app/api/users/logout/route.ts
@@ -0,0 +1,27 @@
+import { connectDB } from "@/db/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+
+connectDB();
+
+export const GET = async (request: NextRequest, response: NextResponse) => {
+  try {
+    //! 1) Create a Response 😘
+    const response = NextResponse.json({
+      msg: "User Logout successfully",
+      success: true,
+      status: 200,
+    });
+
+    //! 2) Clear the token cookie 🍪
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    //! 3) Return the response 🚀
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
