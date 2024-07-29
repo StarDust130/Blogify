15394a5b2b473e4622ccf028c924b6418aea9c71
ChandraShekhar <csyadav0513@gmail.com>
2024-07-29 14:14:07 +0530

    chore: Update error page layout and add error image

--- app/error.tsx
+++ app/error.tsx
@@ -1,30 +1,44 @@
 "use client";
 
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
+export default function Error({}: {
+
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
+    <div className="flex justify-center items-center h-screen flex-col gap-6 ">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="text-center flex justify-center items-center flex-col gap-6"
       >
-        Try again
-      </button>
+        <h2 className="text-4xl font-bold mb-4">Oops! Something went wrong!</h2>
+        <div className="">
+          <Image src="/error.avif" height={300} width={300} alt="Error" />
+        </div>
+        <p className="text-lg mb-6">
+          We are working to fix the problem. Please try again later.
+        </p>
+        <Link href={"/"}>
+          <Button className="mt-5" variant={"link"}>
+            <ArrowLeft className="mr-1" size={16} />
+            Go Back Home
+          </Button>
+        </Link>
+      </motion.div>
+      <div className="absolute inset-0 pointer-events-none flex justify-center items-center">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.2 }}
+          transition={{ duration: 1 }}
+          className="w-1/2 h-1/2  rounded-full blur-2xl"
+        />
+      </div>
     </div>
   );
 }
